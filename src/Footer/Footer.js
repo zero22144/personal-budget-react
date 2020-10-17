456b00f275fcdc56042b9372f4c9706178773f5f
zero22144 <zero22144@users.noreply.github.com>
2020-10-17 15:00:55 -0400

    Added PB code

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
